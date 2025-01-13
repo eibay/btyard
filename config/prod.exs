598da9f8a47ae4ee7aaf62f442db451d192ccbf9
eibay <eibayan@gmail.com>
2025-01-13 18:11:53 +1100

    fix prod config

--- config/prod.exs
+++ config/prod.exs
@@ -18,7 +18,7 @@ config :logger, level: :info
 
 # Runtime production configuration, including reading
 # of environment variables, is done on config/runtime.exs.
-config :btyard, Btyard.Repo,
+# config :btyard, Btyard.Repo,
   # url: System.get_env("DATABASE_URL"),
   # pool_size: String.to_integer(System.get_env("POOL_SIZE") || "10"),
   # ssl: true,
